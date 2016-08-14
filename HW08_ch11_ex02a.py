d1ef16103bbe4b2935be3388b553d64643fe433d
peter <peter@peters-Mac-Pro.local>
2016-08-14 13:29:12 -0700

    done

--- HW08_ch11_ex02a.py
+++ HW08_ch11_ex02a.py
@@ -29,29 +29,36 @@ pledge_histogram = {}
 def histogram_old(s):
     d = dict()
     for c in s:
-        if c not in d:
-            d[c] = 1
-        else:
-            d[c] += 1
+        count = d.get(c, 0)
+        d[c] = count + 1
     return d
 
 
 def histogram_new(s):
-    ...
+    d = dict()
+
+    for c in s:
+        d[c] = 1
+
+    for c in s:
+        d[c] = (d.get(c)) + 1
+
+    return d
 
 
 def get_pledge_list():
     """ Opens pledge.txt and converts to a list, each item is a word in
     the order it appears in the original file. returns the list.
     """
-    # Your code here.
-    pass
-    # return pledge_list (uncomment this)
+    with open('pledge.txt', 'r') as f:
+        pledge_list = f.read().split()
+
+    return pledge_list
 
 
 ###############################################################################
 def main():  # DO NOT CHANGE BELOW
     print(histogram_new(get_pledge_list()))
-
+    print(histogram_old(get_pledge_list()))
 if __name__ == '__main__':
     main()
